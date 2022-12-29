31fcb0640a36a75f33b284926bbbafacc84f15c8
OliverVea <oliver.vea@gmail.com>
2022-12-29 05:37:48 +0100

    Fixing dotnet format whitespace issue

--- src/Application/Services/SynthesizingService.cs
+++ src/Application/Services/SynthesizingService.cs
@@ -18,7 +18,7 @@ public class SynthesizingService : ISynthesizingService
         double offset)
     {
         var sampleBuffer = new double[sampleCount];
-        
+
         _waveformHelper.GenerateSamples(
             sampleBuffer,
             synthesizerConfiguration.SampleRate,
