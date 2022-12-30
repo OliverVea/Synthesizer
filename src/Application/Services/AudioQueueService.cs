2f269d1c51faec02adad483623115a44696fa276
OliverVea <oliver.vea@gmail.com>
2022-12-30 17:22:40 +0100

    Creating AudioQueueService

---
+++ src/Application/Services/AudioQueueService.cs
@@ -0,0 +1,7 @@
+﻿using Synthesizer.Domain.Services;
+
+namespace Synthesizer.Application.Services;
+
+public class AudioQueueService : IAudioQueueService
+{
+}
---
+++ src/Domain/Services/IAudioQueueService.cs
@@ -0,0 +1,8 @@
+﻿namespace Synthesizer.Domain.Services;
+
+/// <summary>
+/// Service for managing and using audio queues.
+/// </summary>
+public interface IAudioQueueService
+{
+}
