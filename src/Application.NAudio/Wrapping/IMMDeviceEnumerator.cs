f81f485bf7725daed740f21459ad345aa663282f
OliverVea <oliver.vea@gmail.com>
2023-01-01 20:45:54 +0100

    Added implementations of wrapper classes

--- src/Application.NAudio/Wrapping/IMMDeviceEnumerator.cs
+++ src/Application.NAudio/Wrapping/IMMDeviceEnumerator.cs
@@ -5,5 +5,5 @@ namespace Application.NAudio.Wrapping;
 // ReSharper disable once InconsistentNaming
 public interface IMMDeviceEnumerator
 {
-    IMMDeviceCollection EnumerateAudioEndPoints(DataFlow render, DeviceState active);
+    IMMDeviceCollection EnumerateAudioEndPoints(DataFlow dataFlow, DeviceState deviceState);
 }
---
+++ src/Application.NAudio/Wrapping/WrappedMMDeviceCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NAudio.CoreAudioApi;
+
+namespace Application.NAudio.Wrapping;
+
+// ReSharper disable once InconsistentNaming
+public class WrappedMMDeviceCollection : WrappedBase<MMDeviceCollection>, IMMDeviceCollection
+{
+    private readonly IEnumerable<IMMDevice> _content;
+
+    public WrappedMMDeviceCollection(MMDeviceCollection sourceObject) : base(sourceObject)
+    {
+        _content = sourceObject.Select(x => new WrappedMMDevice(x));
+    }
+
+    public IEnumerator<IMMDevice> GetEnumerator()
+    {
+        return _content.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
